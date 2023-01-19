8df8e6f798c228e3b5260e083abde7126fce8450
Luigi Capogrosso <luigi@capogrosso.eu>
2023-01-19 18:16:03 +0100

    Add the layers of the SEI framework

---
+++ src/receiver.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import os
+import json
+
+
+def receiver(save_dir,
+             in_filename="out_netsim.json",
+             out_filename="out_receiver.json"):
+    with open(os.path.join(save_dir, in_filename), "r") as f:
+        data = json.load(f)
+
+    size = 0
+    time = 0
+    last_idx = -1
+    packet_idx = 0
+    fdvca_enc_array = []
+
+    for d in data["array"]:
+        curr_idx = d["number"]
+
+        if curr_idx > last_idx:
+            size = size + d["size"]
+            if d["time"] != "inf":
+                time = d["time"]
+            last_idx = curr_idx
+        else:
+            fdvca_enc_array.append({"number":packet_idx,
+                                    "time":time,
+                                    "size":size,
+                                    "data":"none"})
+            packet_idx = packet_idx + 1
+            size = d["size"]
+            if d["time"] != "inf":
+                time = d["time"]
+            last_idx = curr_idx
+
+    fdvca_enc_array.append({"number":packet_idx,
+                            "time":time,
+                            "size":size,
+                            "data":"none"})
+
+    # Create the output file content.
+    fdvca_enc_result = {"encoding":"file",
+                        "file":"filename.xyz",
+                        "array":fdvca_enc_array}
+
+    # Save the output file.
+    with open(os.path.join(save_dir, out_filename), "w") as f:
+        json.dump(fdvca_enc_result, f, ensure_ascii=False, indent=4)
+
+
+if __name__ == "__main__":
+    pass
---
+++ src/sensing.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import os
+import json
+import pickle
+import logging
+
+
+# Create logger.
+logger = logging.getLogger("FDVCA_compute")
+logger.setLevel(logging.DEBUG)
+
+# Create console handler and set level to debug.
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("%(levelname)s: %(message)s")
+
+# Add formatter to ch and ch to logger.
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+
+def compute(save_dir, test_path, out_filename="out_compute.json"):
+    logger.debug(f"Loading the {test_path} data [...]")
+
+    # Get the application data.
+    with open(test_path, "rb") as f:
+        fdvca_enc_array = pickle.load(f)
+
+    logger.debug(f"Successfully loaded {test_path} data.")
+
+    # Create the output file content.
+    fdvca_enc_result = {"encoding":"file",
+                        "file":"filename.xyz",
+                        "array":fdvca_enc_array}
+
+    # Save the output file.
+    with open(os.path.join(save_dir, out_filename), "w") as f:
+        json.dump(fdvca_enc_result, f, ensure_ascii=False, indent=4)
+
+
+if __name__ == "__main__":
+    pass
